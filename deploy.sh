69f5f6bba6a85abb844eb86b4e3b3528fcd4e739
lizheng <mstonelee@qq.com>
2019-04-21 11:59:01 +0800

    commit the deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+killTomcat()
+{
+	pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+	echo "tomcat Id list:$pid"
+	if [ "$pid" = "" ]
+	then 
+		echo "no tomcat pid alive"
+	else
+	    kill -9 $pid
+	fi
+}
+cd $PROJ_PATH/order
+mvn clean install
+
+killTomcat
+
+rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
+rm -f $TOMCAT_APP_PATH/webapps/Seckill.war
+
+cp $PROJ_PATH/Seckill/target/Seckill.war $TOMCAT_APP_PATH/webapps
+
+cd $TOMCAT_APP_PATH/webapps/
+mv Seckill.war ROOT.war
+
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
