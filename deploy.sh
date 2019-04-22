24e49b6c90943bcf92237635edbfc023e9d173bf
lizheng <mstonelee@qq.com>
2019-04-22 20:37:09 +0800

    sdfa

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #编译+部署order站点
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
@@ -6,14 +6,13 @@
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 ### base 函数
-killTomcat(){
-pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
-    fi
-}
+
 cd $PROJ_PATH/Seckill
+mvn clean install
+# 停tomcat
+killTomcat
+# 删除原有工程
+rm -f $TOMCAT_APP_PATH/webapps/Seckill.war
+# 复制新的工程
+cp $PROJ_PATH/Seckill/target/Seckill.war $TOMCAT_APP_PATH/webapps/
+
